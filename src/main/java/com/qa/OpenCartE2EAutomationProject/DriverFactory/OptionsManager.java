45d6a0b5d98389612fbb230618a19ca0e8ec0aac
TapashiRoy <tsharmaroy@gmail.com>
2024-03-06 00:27:11 +0530

    Added Selenoid changes

--- src/main/java/com/qa/OpenCartE2EAutomationProject/DriverFactory/OptionsManager.java
+++ src/main/java/com/qa/OpenCartE2EAutomationProject/DriverFactory/OptionsManager.java
@@ -1,5 +1,7 @@
 package com.qa.OpenCartE2EAutomationProject.DriverFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -18,8 +20,19 @@ public class OptionsManager {
 
 	public ChromeOptions getChromeOptions() {
 		co = new ChromeOptions();
+		
 		if (Boolean.parseBoolean(prop.getProperty("headless").trim()))co.addArguments("--headless");
 		if (Boolean.parseBoolean(prop.getProperty("incognito").trim()))co.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {
+			co.setCapability("browserName", "chrome");			
+			co.setBrowserVersion(prop.getProperty("browserversion").trim());
+			
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);			
+			co.setCapability("selenoid:options", selenoidOptions);			
+		} 			
 		return co;
 	}
 
@@ -32,8 +45,21 @@ public class OptionsManager {
 
 	public FirefoxOptions getFireFoxOptions() {
 		fo = new FirefoxOptions();
+		
 		if (Boolean.parseBoolean(prop.getProperty("headless").trim()))fo.addArguments("--headless");
 		if (Boolean.parseBoolean(prop.getProperty("incognito").trim()))fo.addArguments("--incognito");
+		
+		if(Boolean.parseBoolean(prop.getProperty("remote"))) {			
+			fo.setCapability("browserName", "firefox");
+			fo.setBrowserVersion(prop.getProperty("browserversion").trim());
+			
+			Map<String, Object> selenoidOptions = new HashMap<>();
+			selenoidOptions.put("screenResolution", "1280x1024x24");
+			selenoidOptions.put("enableVNC", true);
+			selenoidOptions.put("name", prop.getProperty("testname"));
+			fo.setCapability("selenoid:options", selenoidOptions);
+		}
+		
 		return fo;
 	}
 }
--- src/test/java/com/qa/OpenCartE2EAutomationProject/Base/BaseTest.java
+++ src/test/java/com/qa/OpenCartE2EAutomationProject/Base/BaseTest.java
@@ -32,14 +32,15 @@ public class BaseTest {
 	protected Registerpage registerPage;
 	protected ViewCartPopuppage vcPage;
 
-	@Parameters({"browser"})
+	@Parameters({"browser", "browserversion"})
 	@BeforeTest
-	public void setup(String browserName) {
+	public void setup(String browserName, String browserVersion) {
 		df = new DriverFactory();
 		prop = df.initProp();
 		
 		if(browserName!=null) {
 			prop.setProperty("browser", browserName);
+			prop.setProperty("browserversion", browserVersion);
 		}		
 		
 		driver = df.initDriver(prop);
